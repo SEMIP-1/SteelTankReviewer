84057ced2330f88642c67c683f3967bb222ec0a1
Salah Eldin Mohamed <salah.ahmed99@eng-st.cu.edu.eg>
2025-12-18 15:37:36 +0200

    Phase 1 - Item 3: Project repository abstraction

---
+++ SteelTankReviewer.Application/Abstractions/Persistence/IProjectRepository.cs
@@ -0,0 +1,14 @@
+﻿using SteelTankReviewer.Domain.Projects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SteelTankReviewer.Application.Abstractions.Persistence
+{
+    public interface IProjectRepository
+    {
+        Task AddAsync(Project project, CancellationToken cancellationToken = default);
+
+        Task<IReadOnlyList<Project>> GetByEngineerIdAsync(Guid engineerId, CancellationToken cancellationToken = default);
+    }
+}
